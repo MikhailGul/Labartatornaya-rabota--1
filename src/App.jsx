9f1b9b509c5150889fb1ae7c6c99cf1efe6b06e8
MikhailGhoul <feasahda@yandex.ru>
2024-09-11 10:42:40 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+  const obj = { name: "john", surname: "smit" };
+  
+  return  <p>name: <span>{obj.name}</span> <br></br> surname: <span>{obj.surname}</span></p>
+  }
+
+
+export default App;
